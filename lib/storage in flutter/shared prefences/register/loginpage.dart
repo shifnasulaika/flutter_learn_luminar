13f7df97ac68895307fd48b50abc333cc43575a0
mubashir-cvr <mubashircheruvayoor@gmail.com>
2023-09-08 23:01:48 +0530

    Login Oage Corection

--- lib/storage in flutter/shared prefences/register/loginpage.dart
+++ lib/storage in flutter/shared prefences/register/loginpage.dart
@@ -3,6 +3,12 @@ import 'package:flutter/material.dart';
 import 'package:ngnfnf/passing%20data%20between%20screance/homepage.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+void main() {
+  runApp(const MaterialApp(
+    home: login(),
+  ));
+}
+
 class login extends StatefulWidget {
   const login({super.key});
 
@@ -10,66 +16,67 @@ class login extends StatefulWidget {
   State<login> createState() => _loginState();
 }
 
-
 class _loginState extends State<login> {
- final usname = TextEditingController();
- final pass = TextEditingController();
- late SharedPreferences preferences;
-
- @override
+  final usname = TextEditingController();
+  final pass = TextEditingController();
+  late SharedPreferences preferences;
+  String? username, password;
+  @override
   void initState() {
-   fetchdata();
+    fetchdata();
     super.initState();
   }
 
-  void fetchdata () async{
+  void fetchdata() async {
     preferences = await SharedPreferences.getInstance();
     setState(() {
-      usname = preferences.getString('name')!;
-
-    }); 
+      username = preferences.getString('username');
+      password = preferences.getString('password');
+    });
   }
-
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:Padding(padding: 
-        const EdgeInsets.all(15),
-        child:Column(
-          children: [
-            TextField(
-              controller: usname,
-              decoration: InputDecoration(
-                label:Text("username"),
-                hintText: 'username',
-              ),
-            ),
-            SizedBox(height: 30,),
-            TextField(
-              controller: pass,
-              decoration: InputDecoration(
-                label:Text("password"),
-                hintText: 'password',
-              ),
-            ),
-
-            ElevatedButton(onPressed: () async{
-              preferences  = await SharedPreferences.getInstance();
-              String uname = usname.text;
-              String conpass = pass.text;
-              if(uname == usname && pass == conpass){
-                Navigator.push(context,MaterialPageRoute(builder: (context) => ProductHome()));
-              }
-              else{
-
-                ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Enter a valid information")));
-
-              }
-            }, child: Text("LogIn"))
-          ],
-        )
-      )
-    );
+        body: Padding(
+            padding: const EdgeInsets.all(15),
+            child: Column(
+              children: [
+                TextField(
+                  controller: usname,
+                  decoration: const InputDecoration(
+                    label: Text("username"),
+                    hintText: 'username',
+                  ),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                TextField(
+                  controller: pass,
+                  decoration: const InputDecoration(
+                    label: Text("password"),
+                    hintText: 'password',
+                  ),
+                ),
+                ElevatedButton(
+                    onPressed: () async {
+                      preferences = await SharedPreferences.getInstance();
+                      String uname = usname.text;
+                      String conpass = pass.text;
+                      if (uname == username && pass == password) {
+                        // ignore: use_build_context_synchronously
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => ProductHome()));
+                      } else {
+                        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                            content: Text("Enter a valid information")));
+                      }
+                    },
+                    child: Text("LogIn"))
+              ],
+            )));
   }
 }
--- lib/storage in flutter/shared prefences/register/registeration_page.dart
+++ lib/storage in flutter/shared prefences/register/registeration_page.dart
@@ -1,11 +1,12 @@
-import 'dart:html';
 
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:ngnfnf/storage%20in%20flutter/shared%20prefences/simple%20login%20using%20shared/shared-login.dart';
+import 'package:ngnfnf/storage%20in%20flutter/shared%20prefences/register/loginpage.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-voidmain(){
-  runApp(MaterialApp(home: RegisterationPage(),));
+
+void main() {
+  runApp(const MaterialApp(
+    home: RegisterationPage(),
+  ));
 }
 
 class RegisterationPage extends StatefulWidget {
@@ -16,72 +17,68 @@ class RegisterationPage extends StatefulWidget {
 }
 
 class _RegisterationPageState extends State<RegisterationPage> {
-  
   final uname = TextEditingController();
   final email = TextEditingController();
   final pword = TextEditingController();
-  @override
-
   late SharedPreferences preferences;
-
+  @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar: AppBar(
-      title: const Text("REGISTER")),
-      body:Center(child:Column(
-        children: [
-
-            Padding(padding: const EdgeInsets.all(10.0),
-            child: TextField(
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                hintText: 'name',
-                 label:Text("name"),
+    return Scaffold(
+        appBar: AppBar(title: const Text("REGISTER")),
+        body: Center(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: TextField(
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'name',
+                    label: Text("name"),
+                  ),
+                  controller: uname,
+                ),
               ),
-              controller: uname,
-
-            ),),
-
-            Padding(padding: const EdgeInsets.all(10),
-            child:TextField(
-              controller: email,
-              decoration: const InputDecoration(
-                label:Text("email"),
-                hintText: 'email'
+              Padding(
+                padding: const EdgeInsets.all(10),
+                child: TextField(
+                  controller: email,
+                  decoration: const InputDecoration(
+                      label: Text("email"), hintText: 'email'),
+                ),
               ),
-            ),),
-            
-            Padding(padding: const EdgeInsets.all(10.0),
-            child: TextField(
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                 label:Text("passsword"),
-                hintText: 'password'
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: TextField(
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      label: Text("passsword"),
+                      hintText: 'password'),
+                  controller: pword,
+                ),
               ),
-              controller: pword,
-            ),
-            ),
-             ElevatedButton(onPressed: ()async{
-              preferences=await SharedPreferences.getInstance();
-              String name= uname.text;
-              String username=email.text;
-              String password=pword.text;
-              if(
-                name !=""&& username  !=""&& password!=""
-              ){
-              preferences.setString('name',name);
-              preferences.setString('username',username);
-               preferences.setString('password',password);
-              preferences.setBool('regitor', false);
-              Navigator.push(context,MaterialPageRoute(builder: (context)=> loginShared()));}
-              uname.text="";
-              email.text="";
-               pword.text="";
-            }, child: const Text("REGISTOR"))
-
-      ],),)
-
-      );
-
+              ElevatedButton(
+                  onPressed: () async {
+                    preferences = await SharedPreferences.getInstance();
+                    String name = uname.text;
+                    String username = email.text;
+                    String password = pword.text;
+                    if (name != "" && username != "" && password != "") {
+                      preferences.setString('name', name);
+                      preferences.setString('username', username);
+                      preferences.setString('password', password);
+                      preferences.setBool('regitor', false);
+                      // ignore: use_build_context_synchronously
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => const login()));
+                    }
+                    uname.text = "";
+                    email.text = "";
+                    pword.text = "";
+                  },
+                  child: const Text("REGISTOR"))
+            ],
+          ),
+        ));
   }
 }
